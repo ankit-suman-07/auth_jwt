a890099026b8149e9965b9eceaaf39948c212190
Ankit Suman <105882103+ankit-suman-07@users.noreply.github.com>
2025-04-21 21:56:42 +0530

    Setup frontend code

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Demo Header
+      </header>
+      Main App
+    </div>
+  );
+}
+
+export default App;
